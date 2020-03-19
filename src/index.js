c1e50e8a23413d9b6117f3bd97c049c6d1285f2b
alexei199617 <alexei199318@mail.ru>
2020-03-20 01:53:19 +0300

    fix: update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 var z = 0 // max
 var g = 0 // min
 var y = 0 // avg
-var min = exports.min = function min (array) {
+exports.min = function min (array) {
     for (let x=0; x < array.length ; x++) {
-        if (g > array) {
-            g = array;
+        if (g > array[x]) {
+            g = array[x];
         }
     }
-    return z;
+    return g;
 }
-
 exports.max = function max (array) {
     for (let x=0; x < array.length ; x++) {
-        if (z < array) {
-            z = array;
+        if (z < array[x]) {
+            z = array[x];
         }
     }
     return z;
 }
-
 exports.avg = function avg (array) {
     for (let x=0; x < array.length ; x++) {
         y += array[x];
     }
     return y;
 }
+const minValue = min([2,-24,13,-11,16,-12,-16,24,13,-38,19,-16,19,3,2,32,0,-10,-19,-27]);
+console.log(minValue);
