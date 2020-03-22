eac96f7e44424a0ec649ebb81f15ead3fbf164bd
alexei199617 <alexei199318@mail.ru>
2020-03-22 21:58:51 +0300

    fix: add new solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
-var z = 0 // max
-var g = 0 // min
-var y = 0 // avg
-exports.min = function min (array) {
-    for (let x=0; x < array.length ; x++) {
-        if (g > array[x]) {
-            g = array[x];
-        }
-    }
-    return g;
-}
-exports.max = function max (array) {
-    for (let x=0; x < array.length ; x++) {
-        if (z < array[x]) {
-            z = array[x];
-        }
-    }
-    return z;
-}
-exports.avg = function avg (array) {
-    for (let x=0; x < array.length ; x++) {
-        y += array[x];
-    }
-    return y;
-}
-const minValue = min([2,-24,13,-11,16,-12,-16,24,13,-38,19,-16,19,3,2,32,0,-10,-19,-27]);
-console.log(minValue);
+exports.min = function min(array) {
+    return !array || !(array.length) ? 0 :
+        array.reduce((accum, value) =>
+            Math.min(accum, value)
+        );
+};
+
+exports.max = function max(array) {
+    return !array || !(array.length) ? 0 :
+        array.reduce((accum, value) =>
+            Math.max(accum, value)
+        );
+};
+
+exports.avg = function avg(array) {
+    return !array || !(array.length) ? 0 :
+    array.reduce((accum, value) => accum+value) / array.length;
+};
